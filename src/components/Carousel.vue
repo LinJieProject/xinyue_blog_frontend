c1c5e367bbb5f2b71feb2b185304764bc5abf607
lddz <2603792595@qq.com>
2019-10-23 15:28:06 +0800

    完成轮播图组件

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,10 +1,9 @@
 <!-- 轮播图组件 -->
 <template>
   <div>
-<el-carousel type="card" height="200px">
+<el-carousel type="card" height="300px">
       <el-carousel-item v-for="item in img_srcs" :key="item.id">
-          <img src=item.src >
-        <!-- <h3 class="small">{{ item }}</h3> -->
+          <img :src="item.src" >
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -18,9 +17,9 @@ export default {
   data() {
     return {
        img_srcs:[
-           {id:1,src:'../assets/imgs/图片01.jpg'},
-           {id:2,src:'../assets/imgs/图片01.jpg'},
-           {id:3,src:'../assets/imgs/图片01.jpg'},
+           {id:1,src:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1571825663619&di=84a68e81d6fcd3754ea89bac3996c787&imgtype=0&src=http%3A%2F%2Fnews.mydrivers.com%2Fimg%2F20120423%2F2012042308530117.jpg"},
+           {id:2,src:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1571825663619&di=84a68e81d6fcd3754ea89bac3996c787&imgtype=0&src=http%3A%2F%2Fnews.mydrivers.com%2Fimg%2F20120423%2F2012042308530117.jpg"},
+           {id:3,src:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1571825663619&di=84a68e81d6fcd3754ea89bac3996c787&imgtype=0&src=http%3A%2F%2Fnews.mydrivers.com%2Fimg%2F20120423%2F2012042308530117.jpg"},
        ]
     };
   },
