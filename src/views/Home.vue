cae50434e441f4274e0de73dd7b4148071ea29da
lddz <260379295@qq.com>
2019-10-25 22:40:41 +0800

    使用获取所有文章基本信息API，现在首页可以浏览来自数据库的文章列表

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,15 +24,21 @@ export default {
   props: {},
   data() {
     return {
-      articles:[
-        {id:1,title:"用 Git 作为聊天应用的后端",summary:"从去年实习到现在8月，工作大半年，正式工作前，也把自己那笨重的神州战神游戏本切换到了今天四月推出的华为 MateBook 14，3:2超薄体验"},
-        {id:2,title:"用 Git 作为聊天应用的后端",summary:"从去年实习到现在8月，工作大半年，正式工作前，也把自己那笨重的神州战神游戏本切换到了今天四月推出的华为 MateBook 14，3:2超薄体验"},
-        {id:3,title:"用 Git 作为聊天应用的后端",summary:"从去年实习到现在8月，工作大半年，正式工作前，也把自己那笨重的神州战神游戏本切换到了今天四月推出的华为 MateBook 14，3:2超薄体验"},
-        {id:4,title:"用 Git 作为聊天应用的后端",summary:"从去年实习到现在8月，工作大半年，正式工作前，也把自己那笨重的神州战神游戏本切换到了今天四月推出的华为 MateBook 14，3:2超薄体验"},
-      ]
+      articles: []
     };
   },
-  created() {},
+  created() {
+    // 变量“vm”存储该组件实例
+    var vm=this
+    fetch("http://127.0.0.1:9090/Article")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(jsonData) {
+        vm.articles = jsonData.data;
+        // console.log(vm.articles);
+      });
+  },
   mounted() {},
   activited() {},
   update() {},
