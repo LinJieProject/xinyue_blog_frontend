93c43a9d9eecf5086cd4f4d48747cf1f8fe23618
lddz <2603792595@qq.com>
2019-10-28 18:08:30 +0800

    继续编写登录功能

--- src/views/LoginAndRegistration.vue
+++ src/views/LoginAndRegistration.vue
@@ -7,12 +7,12 @@
       <el-col :span="8" :offset="8">
         <div class="grid-content bg-purple-light">
           <el-main>
-            <el-form ref="form" :model="form" label-width="80px">
-              <el-form-item label="账号">
-                <el-input v-model="form.name"></el-input>
+            <el-form action="" method="POST" ref="form" :model="form" label-width="80px">
+              <el-form-item label="用户名">
+                <el-input name="username" v-model="form.name"></el-input>
               </el-form-item>
               <el-form-item label="密码">
-                <el-input type="password" v-model="form.pass"></el-input>
+                <el-input name="password" type="password" v-model="form.pass"></el-input>
               </el-form-item>
               <el-form-item>
                 <el-button type="primary" @click="onSubmit">登录</el-button>
@@ -47,12 +47,15 @@ export default {
   update() {},
   beforeRouteUpdate() {},
   methods: {
+    loggg(){
 
+    },
     onSubmit() {
       //将"key1=value1&key2=valu2" 形式封装整FromData形式
       let formData = new FormData();
-      formData.append("username", "hello");
-      formData.append("password", "1111aaaa");
+      formData.append("username", "tom");
+      formData.append("password", "123");
+      console.log(formData);
 
       var opts = {
         method: "POST", //请求方法
@@ -60,15 +63,21 @@ export default {
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json"
-        }
+        },
+        // body: {
+        //   //post请求参数
+        //   username: "Hubot",
+        //   password: "hubot"
+        // }
       };
       fetch("http://127.0.0.1:9090/Login", opts)
         .then(response => {
           //你可以在这个时候将Promise对象转换成json对象:response.json()
           //转换成json对象后return，给下一步的.then处理
-          return response.text;
+          return response.text();
         })
         .then(responseText => {
+          console.log(responseText);
           alert(responseText);
         })
         .catch(error => {
